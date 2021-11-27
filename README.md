4310643445bde14429ffa341c8bbc44bfa3d3995
antoinedelia <antoine.delia@gmail.com>
2021-11-27 22:20:22 +0100

    Download files to temp folder

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ Automatic generation of video with top posts from a subreddit.
 
 ```shell
 $ pip install -r requirements.txt
+
+$ echo REDDIT_CLIENT_ID=YOUR_REDDIT_CLIENT_ID >> .env
+$ echo REDDIT_CLIENT_SECRTET=YOUR_REDDIT_CLIENT_SECRET >> .env
+$ echo REDDIT_USER_AGENT=SOME_USER_AGENT >> .env
+
 $ python src/main.py --subreddit=funny --output=output.mp4
 ```
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import argparse
 import os
-from warnings import resetwarnings
+import shutil
 from reddit import Reddit
 from dotenv import load_dotenv
 from loguru import logger
+import requests
 
 load_dotenv()
 
@@ -11,7 +12,29 @@ REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 
+TEMP_FOLDER = "src/temp"
+
 parser = argparse.ArgumentParser(description="Reddit Best Of Generator")
+
+
+def download(id: str, url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    _, file_extension = os.path.splitext(url)
+    filename = id + file_extension
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:
+        logger.warning("Download failed: status code {}\n{}".format(r.status_code, r.text))
 
 
 def main():
@@ -52,24 +75,46 @@ def main():
     nsfw = args.nsfw
     posts_limit = args.posts_limit
 
+    logger.info(f"Getting {posts_limit} {type} posts from /r/{subreddit}")
+    if type == "top":
+        logger.info(f"Top posts will be filtered by {time}")
+    logger.info(f"NSFW content will be {'included' if nsfw else 'excluded'}")
+
     # 1 - Get the posts from Reddit
     reddit = Reddit(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT)
     if type == "hot":
         posts = reddit.get_hot_posts(subreddit, posts_limit, nsfw)
-        for post in posts:
-            if f"https://www.reddit.com{post.permalink}" == post.url:
-                logger.info(f"Post {post.title} is not a media")
-            else:
-                logger.info(f"This post with score {post.score} and title {post.title}: {post.url}. Selftext: {post.selftext}")
     elif type == "top":
         posts = reddit.get_top_posts(subreddit, time, posts_limit, nsfw)
 
+    logger.info(f"{len(posts)} posts found!")
+
+    filtered_posts = []
+    # We filter out posts that are self posts (not linking to a media such as image or video)
+    for post in posts:
+        if f"https://www.reddit.com{post.permalink}" == post.url or post.url.startswith("https://www.reddit.com"):
+            logger.info(f"Post {post.title} is not a media, skipping it.")
+            continue
+        filtered_posts.append(post)
+
+    logger.info(f"{len(filtered_posts)} media posts found!")
+
     # 2 - Download the media from the posts
-    # https://stackoverflow.com/questions/56950987/download-file-from-url-and-save-it-in-a-folder-python
+    for post in filtered_posts:
+        url = post.url
+        if post.url.startswith("https://v.redd.it"):
+            url = post.__dict__["secure_media"]["reddit_video"]["fallback_url"]
+            url = url.split("?")[0]  # remove query params
+
+        logger.info(f"Downloading {url}")
+        download(post.id, url, TEMP_FOLDER)
 
     # 3 - Create the video by combining the media
 
     # 4 - Upload the media to Youtube
+
+    # 5 - Clean up the temp folder
+    shutil.rmtree(TEMP_FOLDER)
 
 
 if __name__ == "__main__":
